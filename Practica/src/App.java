10baad5fd55b83d2526871e452a6b265ac525c41
Oscar Acedo <daw202401@estudiantes.salesianasnsp.es>
2023-03-13 13:15:04 +0100

    Segunda prueba

--- Practica/src/App.java
+++ Practica/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Practica 12, primera prueba");
+        System.out.println("Practica 12, segunda prueba");
     }
 }
